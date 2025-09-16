936a473274f42786dc391a22d8036eb5cdbdd449
mitul02ara <ismataramitul@gmail.com>
2025-09-16 11:04:12 +0600

    mitul

--- secant.cpp
+++ secant.cpp
@@ -1,7 +1,23 @@
+
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
-  return 0;
+// Define the function f(x) = e^x - 3x
+double f(double x) {
+    return exp(x) - 3*x;
 }
+
+
+int main() {
+    double x1, x2 ,tol;
+    cout<<"enter guess-1 guess-2 and error-tolerance : ";
+    cin >> x2;
+    cin >> x1;
+    cin>> tol;
+    int maxIter = 50;
+
+    double root = secant(x1, x2, tol, maxIter);
+    cout << "\nApproximate root (Secant Method): " << root << endl;
+    return 0;}
